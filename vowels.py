af71897e77ff7a9fcfe2312c081d9c75b687c6ee
jarobystrzyca <jarodinoopole@gmail.com>
2026-05-24 02:54:51 +0200

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,20 @@
+word = str(input("Input your word: "))
+print(f"The word {word} has: ")
+for vow in range(1):
+    a = word.count('a')
+    print(f"{a} a")
+for vow in range(1):
+    e= word.count('e')
+    print(f"{e} e")
+for vow in range(1):
+    i = word.count('i')
+    print(f"{i} i")
+for vow in range(1):
+    o = word.count('o')
+    print(f"{o} o")
+for vow in range(1):
+    u = word.count('u')
+    print(f"{u} u")
+for vow in range(1):
+    y = word.count('y')
+    print(f"{y} y")
